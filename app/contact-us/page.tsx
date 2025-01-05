ee94cf00f5635f9c119197b81e0f56c370c142e5
junaied hossain <junaied.hossain@flytesolutions.com>
2025-01-05 18:50:59 +0600

    meta tags added

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Contact from '../component/Contact/Contact';
 
+export const metadata = {
+    title: "Contact Us | Flyte Solutions Ltd.",
+    description: "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
+  };
 const page = () => {
     return (
         <div className=' container mx-auto'>
