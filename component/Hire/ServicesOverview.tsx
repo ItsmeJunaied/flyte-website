84fe35c6190a3f766921497d7284029d7e3b8709
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-01-12 12:37:25 +0600

    added dummy data for hire page and added serviceoverview and fit highlight and book consultation in hire page

--- component/Hire/ServicesOverview.tsx
+++ component/Hire/ServicesOverview.tsx
@@ -1,20 +1,16 @@
 import React from "react";
+import { serviceOverviewData } from "../../api/Dummy";
 
-// type alias for icon 
-type Icon = {
-  path: string;
-  fill: string;
-}
-
-type ServiceOverviewData = {
+type Service = {
   title: string;
   description: string;
-  icon: Icon;
-
+  icon: {
+    path: string;
+    fill: string;
+  };
 }
 
-const ServicesOverview: React.FC<{serviceOverviewData: ServiceOverviewData[]}> = ({serviceOverviewData}) => {
- 
+const ServicesOverview: React.FC = () => {
   return (
     <div className="px-5 lg:px-10 bg-white pb-10">
       <div className=" container mx-auto">
@@ -23,23 +19,22 @@ const ServicesOverview: React.FC<{serviceOverviewData: ServiceOverviewData[]}> =
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {serviceOverviewData?.map((service, index) => (
-          <div key={index} className="h-[281px] p-5 bg-[#f4f5f9] rounded-xl grid grid-rows-3 place-items-center gap-">
-              <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="56"
-                  height="56"
-                  viewBox="0 0 56 56"
-                  fill="none">
-                <path 
-                  d={service?.icon?.path}
-                  fill={service?.icon?.fill}
-                  />
+          {serviceOverviewData?.map((service: Service, index: number) => (
+            <div
+              key={index}
+              className="h-[281px] p-5 bg-[#f4f5f9] rounded-xl grid grid-rows-3 place-items-center"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="56" height="56" viewBox="0 0 56 56" fill="none">
+                <path d={service?.icon?.path} fill={service?.icon?.fill} />
               </svg>
-              <h4 className="text-gray-800 text-base xl:text-xl text-center font-semibold">{service?.title}</h4>
-              <p className="text-gray-600 text-sm font-normal text-center self-start">{service?.description}</p>
-          </div>
-            ))}
+              <h4 className="text-gray-800 text-base xl:text-xl text-center font-semibold">
+                {service?.title}
+              </h4>
+              <p className="text-gray-600 text-sm font-normal text-center self-start">
+                {service?.description}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
