eacdf49e6afb26c4535e214b009592d7cf1065b7
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-01-14 15:14:15 +0600

    added mission and vision

--- app/company/about-us/page.tsx
+++ app/company/about-us/page.tsx
@@ -1,12 +1,17 @@
-import AboutBanner from '@/component/Company/AboutUs/AboutBanner';
-import MissionVision from '@/component/Company/AboutUs/MissionVision';
-import React from 'react';
+import AboutBanner from "@/component/Company/AboutUs/AboutBanner";
+import MissionVision from "@/component/Company/AboutUs/MissionVision";
+import React from "react";
+
+export const metadata = {
+  title: "Company | Flyte Solutions Ltd.",
+  description: "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
+};
 
 const page = () => {
   return (
     <div>
-      <AboutBanner/>
-      {/* <MissionVision/> */}
+      <AboutBanner />
+      <MissionVision />
     </div>
   );
 };
--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import CommingSoon from '../../component/Common/CommingSoon';
+import React from "react";
+import AboutBanner from "@/component/Company/AboutUs/AboutBanner";
+import MissionVision from "@/component/Company/AboutUs/MissionVision";
 
 export const metadata = {
-    title: "Company | Flyte Solutions Ltd.",
-    description: "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
-  };
+  title: "Company | Flyte Solutions Ltd.",
+  description: "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
+};
 const page = () => {
-    return (
-        <div>
-            <CommingSoon/>
-        </div>
-    );
+  return (
+    <div>
+      <AboutBanner />
+      <MissionVision />
+    </div>
+  );
 };
 
 export default page;
