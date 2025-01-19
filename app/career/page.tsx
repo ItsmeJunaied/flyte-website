f38254f9e34f5502315bb5ea00b8d94afd6a9dd4
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-01-19 15:31:14 +0600

    created unlock protential component

--- app/career/page.tsx
+++ app/career/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import CareerBanner from '@/component/Career/CareerBanner';
+import UnlockPotential from '@/component/Career/UnlockPotential';
 
 const page = () => {
     return (
         <div>
             <CareerBanner/>
+            <UnlockPotential/>
         </div>
     );
 };
