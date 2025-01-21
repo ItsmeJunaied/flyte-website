8b46378178b7b3d2b2b9245a7f907d995f3ab50d
junaied hossain <junaied.hossain@flytesolutions.com>
2025-01-21 18:44:53 +0600

    aa

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Contact from "../component/Contact/Contact";
 import OperationsSections from "../component/Home/OperationsSections";
 import ProjectSection from "../component/Home/ProjectSection";
 import AboutUs from "../component/Home/AboutUs";
+import ProductsHomeComp from "@/component/Home/ProductsHomeComp";
 // import ServiceMarquee from "../component/Home/ServiceMarquee";
 const page = () => {
   // console.log(brandData)nlkmkj
