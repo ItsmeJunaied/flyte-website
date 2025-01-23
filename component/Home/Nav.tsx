3c26416ebaab9fc63fbcf99c6c1114c6f973328a
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-01-23 12:49:49 +0600

    remove color from nev for mobile view

--- component/Home/Nav.tsx
+++ component/Home/Nav.tsx
@@ -23,7 +23,7 @@ const Nav: React.FC<{ navData: NavData }> = ({ navData }) => {
 
   // console.log("pathname", pathname.split("/")[1])
   return (
-    <div className="mt-14 md:mt-[92px]">
+    <div className="mt-14 lg:mt-[92px]">
       <div className="header">
         <nav className="nav container">
           <div className="nav__data">
@@ -55,10 +55,10 @@ const Nav: React.FC<{ navData: NavData }> = ({ navData }) => {
                           </p>
                           <i className="fa-solid fa-chevron-down fa-2xs"></i>
                         </div>
-                        <div className="dropdown__container bg-[#F4F2F0]">
+                        <div className="dropdown__container">
                           <div className="dropdown__content">
-                            <div className="md:container grid grid-cols-1 md:grid-cols-3">
-                              <div className="md:col-span-1 hidden lg:flex flex-col gap-4 flex-shrink-0 ">
+                            <div className="lg:container grid grid-cols-1 lg:grid-cols-3">
+                              <div className="lg:col-span-1 hidden lg:flex flex-col gap-4 flex-shrink-0 ">
                                 <h1 className="text-lg text-btnColor ">{item.name}</h1>
                                 <p className="text-xs text-[#131313B2]">{item.description}</p>
                                 <Link
@@ -68,19 +68,19 @@ const Nav: React.FC<{ navData: NavData }> = ({ navData }) => {
                                   <p className="">Learn more</p>
                                 </Link>
                               </div>
-                              <div className="md:col-span-2 rounded-lg flex-shrink-0">
-                                <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-2">
+                              <div className="lg:col-span-2 rounded-lg flex-shrink-0">
+                                <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-2">
                                   {item.features?.map((feature, featureIndex) => (
                                     <Link href={feature.path} key={featureIndex}>
-                                      <div className="bg-[#F7FAFF] md:bg-white flex flex-row items-center gap-2 md:gap-4 md:border-2 md:border-white hover:border-btnColor px-5 md:px-4 py-1 md:py-4 md:rounded-lg md:h-24 max-w-[406px]">
+                                      <div className="bg-[#F7FAFF] lg:bg-white flex flex-row items-center gap-2 lg:gap-4 lg:border-2 lg:border-white hover:border-btnColor px-5 lg:px-4 py-1 lg:py-4 lg:rounded-lg lg:h-24 max-w-[406px]">
                                         <div className="w-7 h-7">
                                           <i
-                                            className={`w-7 h-7 fa ${feature.icon} text-lg md:text-3xl text-[#5856d6]`}
+                                            className={`w-7 h-7 fa ${feature.icon} text-lg lg:text-3xl text-[#5856d6]`}
                                           ></i>
                                         </div>
                                         <div>
-                                          <p className="text-btnColor text-xs md:text-base">{feature.name}</p>
-                                          <p className=" text-[#131313B2] text-xs hidden md:block">
+                                          <p className="text-btnColor text-xs lg:text-base">{feature.name}</p>
+                                          <p className=" text-[#131313B2] text-xs hidden lg:block">
                                             {feature.description}
                                           </p>
                                         </div>
@@ -89,7 +89,7 @@ const Nav: React.FC<{ navData: NavData }> = ({ navData }) => {
                                   ))}
                                   <div className="bg-[#F7FAFF]">
                                     <Link
-                                      className="nav-close mx-5 bg-btnColor text-white px-3 w-fit rounded md:hidden flex justify-center items-center"
+                                      className="nav-close mx-5 bg-btnColor text-white px-3 w-fit rounded lg:hidden flex justify-center items-center"
                                       href={item.path}
                                     >
                                       Learn more
