a83b52ad2cfe8fd8059adf2312401dcbe22fcc97
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-02-09 12:44:36 +0600

    added reak image for case studies

--- component/CaseStudies/CaseStudies.tsx
+++ component/CaseStudies/CaseStudies.tsx
@@ -16,7 +16,7 @@ const CaseStudies = () => {
             <h1 className="text-zinc-950 text-4xl font-bold  leading-[46px]">
               {title}
             </h1>
-            <p className=" lg:w-[520px] text-[#12094a] text-sm font-normal leading-snug  lg:px-0">
+            <p className="text-[#12094a] text-sm font-normal leading-snug  lg:px-0">
               Explore success stories of businesses transforming challenges into
               opportunities with our solutions.
             </p>
@@ -41,35 +41,35 @@ const CaseStudies = () => {
               <div className="px-2">
                 <img
                   className=" w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-banner.png"
+                  src="/images/case/case1.jpg"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className=" w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-2.png"
+                  src="/images/case/picture-ongold.png"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className=" w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-banner.png"
+                  src="/images/case/hydro-one.png"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className=" w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-2.png"
+                  src="/images/case/pushlocal.jpg"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className=" w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-2.png"
+                  src="/images/case/fsa.png"
                   alt=""
                 />
               </div>
@@ -85,28 +85,28 @@ const CaseStudies = () => {
               <div className="px-2">
                 <img
                   className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-3.png"
+                  src="/images/case/hba.jpg"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-4.png"
+                   src="/images/case/realestate1.png"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-3.png"
+                  src="/images/case/erp.png"
                   alt=""
                 />
               </div>
               <div className="px-2">
                 <img
                   className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-                  src="/images/case-study-img-4.png"
+                   src="/images/case/12.jpg"
                   alt=""
                 />
               </div>
@@ -120,14 +120,14 @@ const CaseStudies = () => {
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-banner.png"
+              src="/images/case/keeferealestate.png"
               alt=""
             />
           </div>
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-img-2.png"
+              src="/images/case/cds.jpg"
               alt=""
             />
           </div>
@@ -141,28 +141,28 @@ const CaseStudies = () => {
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-img-4.png"
+              src="/images/case/bloomfire.jpg"
               alt=""
             />
           </div>
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-img-4.png"
+              src="/images/case/americandesign.jpg"
               alt=""
             />
           </div>
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-img-4.png"
+              src="/images/case/1.jpg"
               alt=""
             />
           </div>
           <div className="px-2">
             <img
               className="w-[250px] lg:w-[350px]  h-[150px] lg:h-[250px] rounded-xl"
-              src="/images/case-study-img-4.png"
+              src="/images/case/clinicaltrial.png"
               alt=""
             />
           </div>
