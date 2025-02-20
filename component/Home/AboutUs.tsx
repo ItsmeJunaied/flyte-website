0d5435adaaa8cd43985f5ede3e9ef5ec83a775e9
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-02-20 10:26:37 +0600

    add url

--- component/Home/AboutUs.tsx
+++ component/Home/AboutUs.tsx
@@ -125,7 +125,7 @@ const AboutUs: React.FC = () => {
 
             {/* Learn More Button */}
             <div className="w-full flex lg:justify-start justify-center lg:items-start items-center">
-              <Link className="w-fit h-fit bgGradientNevyBlue px-[32px] py-[12px] rounded-lg mt-5 text-white" href={"/"}>
+              <Link className="w-fit h-fit bgGradientNevyBlue px-[32px] py-[12px] rounded-lg mt-5 text-white" href={"/company"}>
                 <p>Learn More About Our Business Solutions</p>
               </Link>
             </div>
