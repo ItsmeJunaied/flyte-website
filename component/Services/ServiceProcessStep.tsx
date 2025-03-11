75de1de5cfda4b9c7534c34c6bb0c48f2f1bfea6
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-03-11 13:12:47 +0600

    updated

--- component/Services/ServiceProcessStep.tsx
+++ component/Services/ServiceProcessStep.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import StepCard from "../Common/StepCard";
-import { serviceProcessData } from "@/api/Dummy";
+// import StepCard from "../Common/StepCard";
+// import { serviceProcessData } from "@/api/Dummy";
 
 const ServiceProcessStep = () => {
   return (
     <div>
-      <StepCard data={serviceProcessData} />
+      {/* <StepCard data={serviceProcessData} /> */}
     </div>
   );
 };
