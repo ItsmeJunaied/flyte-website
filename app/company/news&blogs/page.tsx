6602a4a1511209117ab4801340f88a67f5ee6aae
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-03-12 15:20:43 +0600

    added real blogs

--- app/company/news&blogs/page.tsx
+++ app/company/news&blogs/page.tsx
@@ -1,4 +1,4 @@
-import { blogData, NewsBlogsBannerData } from "@/api/Dummy";
+import { NewsBlogsBannerData } from "@/api/Dummy";
 import BlogGlimpsSection from "@/component/Blogs/BlogGlimpsSection";
 import CommonBanner from "@/component/Common/CommonBanner";
 import Contact from "@/component/Contact/Contact";
@@ -7,14 +7,13 @@ import React from "react";
 
 const page = () => {
   const bannerData = NewsBlogsBannerData;
-  const blogsData = blogData;
   return (
     <div>
       <CommonBanner bannerData={bannerData} />
-      <div className=" container py-10">
+      <div className="container py-10">
         <BlogGlimpsSection />
       </div>
-      <BlogSection blogData={blogsData} />
+      <BlogSection />
       <div className="container">
         <Contact />
       </div>
--- redux/api/baseApi.ts
+++ redux/api/baseApi.ts
@@ -6,6 +6,6 @@ const baseQuery = fetchBaseQuery({
 
 export const baseApi = createApi({
   baseQuery,
-  tagTypes: ["HomeBanner", "Contact", "CaseStudies", "Common", "Career"],
+  tagTypes: ["HomeBanner", "Contact", "CaseStudies", "Common", "Career", "Blogs"],
   endpoints: () => ({}),
 });
