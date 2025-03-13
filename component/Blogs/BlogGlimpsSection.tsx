d63613b6fbe9c87b88de72ca8ce57dd038c80654
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-03-13 14:04:02 +0600

    added dynamic trending data of blog page

--- component/Blogs/BlogGlimpsSection.tsx
+++ component/Blogs/BlogGlimpsSection.tsx
@@ -6,6 +6,7 @@ import { blogData } from "@/api/Dummy";
 const BlogGlimpsSection = () => {
   const blogsData = blogData;
 
+
   return (
     <div>
       <div className="flex flex-col lg:flex-row justify-center lg:justify-between items-stretch gap-5">
