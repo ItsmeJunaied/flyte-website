880ee2baa39b7da858080d07db4c48740ad2993f
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-03-18 13:28:54 +0600

    added meta data

--- app/career/[slug]/page.tsx
+++ app/career/[slug]/page.tsx
@@ -1,9 +1,32 @@
 import CareerDetails from "@/component/Career/CareerDetails";
 import React from "react";
+import { Metadata } from "next";
 
 type PageProps = {
   params: Promise<{ slug: string }>;
 };
+
+const fetchCareerData = async (slug: string) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/career/${slug}`);
+  if (!res.ok) return null;
+  return res.json();
+};
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const careerData = await fetchCareerData(slug);
+  // console.log("careerData", careerData?.data)
+  const { meta_title, meta_description } = careerData?.data || {};
+
+  return {
+    title: meta_title || "Career Details | Flyte Solutions Ltd.",
+    description: meta_description || "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
+    openGraph: {
+      title: meta_title || "Career Details | Flyte Solutions Ltd.",
+      description: meta_description || "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
+    },
+  };
+}
 
 // Generate static paths for all case studies
 export async function generateStaticParams() {
@@ -18,7 +41,6 @@ export async function generateStaticParams() {
 
 const page = async ({ params }: PageProps) => {
   const { slug } = await params;
-  console.log("slug", slug);
 
   return (
     <div>
