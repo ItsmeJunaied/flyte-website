08bc7f04f12367c56a6ca8c9778d48cf46ee85e0
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-03-23 13:43:33 +0600

    updated

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -11,7 +11,7 @@ export const metadata = {
     "Get in touch with Flyte Solutions Ltd. for any inquiries or support.",
 };
 
-const page: React.FC = () => {
+const page = () => {
   const productsBannerData = productBannerData;
   return (
     <div>
