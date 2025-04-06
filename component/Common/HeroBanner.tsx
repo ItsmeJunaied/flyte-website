3b05041f5d6938ee8317f3a25394640fc45dc3c2
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-04-06 17:22:12 +0600

    updated

--- component/Common/HeroBanner.tsx
+++ component/Common/HeroBanner.tsx
@@ -13,23 +13,31 @@ const HeroBanner: React.FC<{ bannerData: HeroBannerProps }> = ({ bannerData }) =
   const { title, short_title, short_description, image } = bannerData;
   return (
     <div
-      className="pt-10 lg:pt-40 relative lg:min-h-[610px] bg-cover bg-center"
+      className="pt-10 lg:pt-44 relative lg:min-h-[610px] bg-cover bg-center"
       style={{
-        backgroundImage: `url(${image})`
+        backgroundImage: `url(${image})`,
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-80" />
-      <div className="container pb-8 lg:pb-16 flex flex-col-reverse lg:flex-row gap-8 lg:gap-16 relative">
-        <div className="w-full lg:w-[60%] flex flex-col justify-between space-y-8 lg:space-y-16">
-          <div>
-            <h2 className="text-white text-base lg:text-lg font-bold tracking-wide mb-4">{short_title}</h2>
-            <h1 className="text-white text-2xl lg:text-4xl font-bold leading-10 tracking-wider">{title}</h1>
+      {/* className="container pb-8 lg:pb-16 flex flex-col-reverse lg:flex-row gap-8 lg:gap-16 relative" */}
+      <div className="container pb-8 lg:pb-16 relative">
+        <div className="w-full lg:w-[60%]">
+          <div className="space-y-4 lg:space-y-6">
+            <h4 className="text-[#6ec1ff] text-base lg:text-lg uppercase font-bold tracking-wide flex lg:items-center gap-2">
+              <div className="w-[22px] h-0.5 bg-[#6ec1ff] mt-3 lg:mt-0" /> {short_title}
+            </h4>
+            <h1 className="text-white text-2xl lg:text-4xl font-bold lg:leading-[46px]">{title}</h1>
+            <p className="text-[#dddddd] text-sm leading-snug">{short_description}</p>
           </div>
-          <p className="text-white text-sm lg:text-[18px] tracking-wide">{short_description}</p>
 
-          <Link href="/schedule-consultation" className="w-fit px-8 py-3 bgGradientNevyBlue rounded-md text-white text-base font-semibold">
-            Book A Consultation
-          </Link>
+          <div className="mt-8 lg:mt-12">
+            <Link
+              href="/schedule-consultation"
+              className="w-fit px-8 py-3 bgGradientNevyBlue rounded-md text-white text-base font-semibold"
+            >
+              Book A Consultation
+            </Link>
+          </div>
         </div>
         {/* <div className="w-full lg:w-[60%]">
           <img
