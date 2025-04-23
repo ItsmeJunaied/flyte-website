bcc5276ae7b1a3d0c0eecc71e1b85e5bc795d1f4
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-04-23 11:43:36 +0600

    updated

--- component/Blogs/FavoriteBlogs.tsx
+++ component/Blogs/FavoriteBlogs.tsx
@@ -30,7 +30,7 @@ const FavoriteBlogs = () => {
         <h2 className="mb-4 text-[#161c2d] text-2xl text-center font-bold">You May Also Like</h2>
       </div>
 
-      <div className="container py-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+      <div className=" py-5 grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {blogs?.map((blog: Blog, index: number) => (
           <div
             key={index}
