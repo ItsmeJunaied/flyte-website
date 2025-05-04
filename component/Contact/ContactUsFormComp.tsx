686ad126d438c079dbed8ac3caadffad1e257b85
ishrafil-hossain <ishrafil2233@gmail.ocm>
2025-05-04 18:32:19 +0600

    updated contact form

--- component/Contact/ContactUsFormComp.tsx
+++ component/Contact/ContactUsFormComp.tsx
@@ -31,6 +31,13 @@ const ContactUsFormComp: React.FC = () => {
     formState: { errors },
   } = useForm<Inputs>();
 
+  const totalSize = files.reduce((acc, file) => acc + file.size, 0);
+  const formatBytes = (bytes: number) => {
+    if (bytes < 1024) return `${bytes} B`;
+    if (bytes < 1024 * 1024) return `${(bytes / 1024).toFixed(1)} KB`;
+    return `${(bytes / (1024 * 1024)).toFixed(2)} MB`;
+  };
+
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
     setIsSubmitted(true);
 
@@ -39,6 +46,12 @@ const ContactUsFormComp: React.FC = () => {
       return;
     } else {
       setPhoneValue(true);
+    }
+
+    const maxTotalSize = 25 * 1024 * 1024; // 25MB
+    if (totalSize > maxTotalSize) {
+      toast.error("Total attachment size exceeds 25MB. Please remove some files.");
+      return;
     }
 
     try {
@@ -53,12 +66,12 @@ const ContactUsFormComp: React.FC = () => {
       if (data.attachment && data.attachment[0]) {
         const file = data.attachment[0];
         const allowedTypes = [
-          "image/jpg",
+          "image/jpeg",
           "image/png",
           "application/pdf",
           "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         ];
-        const maxSize = 25 * 1024 * 1024; // 25MB
+        const maxSize = 25 * 1024 * 1024;
 
         if (!allowedTypes.includes(file.type)) {
           toast.error("Invalid file type. Please upload a .jpg, .png, .pdf, or .docx file.");
@@ -73,7 +86,6 @@ const ContactUsFormComp: React.FC = () => {
         postData.append("attachment", file);
       }
 
-      // const response = await submitContactForm(postData);
       const response = await addContact(postData);
       if (response?.error) {
         toast.error(`Error: ${response.error}`);
@@ -81,7 +93,6 @@ const ContactUsFormComp: React.FC = () => {
         toast.success("Your message has been sent successfully!");
         reset();
         setFiles([]);
-        // setIsChecked(false);
         setIsSubmitted(false);
         setIsLoading(false);
       }
@@ -97,7 +108,7 @@ const ContactUsFormComp: React.FC = () => {
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = event.target.files ? Array.from(event.target.files) : [];
-    setFiles((prevFiles) => [...prevFiles, ...(selectedFiles as File[])]);
+    setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
   };
 
   return (
@@ -117,7 +128,7 @@ const ContactUsFormComp: React.FC = () => {
             {...register("name", { required: true })}
             autoComplete="off"
           />
-          {errors.name && <span className=" text-red-600">Name is required</span>}
+          {errors.name && <span className=" text-red-600 text-xs lg:text-sm">Name is required</span>}
         </div>
         <div className="w-full flex-col justify-start items-start gap-2 inline-flex">
           <label className="self-stretch text-[#666666] text-sm lg:text-base font-semibold font-['DM Sans'] leading-[18px]">
@@ -129,7 +140,7 @@ const ContactUsFormComp: React.FC = () => {
             placeholder="Type your company name"
             {...register("company_name", { required: true })}
           />
-          {errors.company_name && <span className=" text-red-600">Comapny Name is required</span>}
+          {errors.company_name && <span className=" text-red-600 text-xs lg:text-sm">Comapny Name is required</span>}
         </div>
       </div>
 
@@ -144,7 +155,7 @@ const ContactUsFormComp: React.FC = () => {
             placeholder="Type your email"
             {...register("email", { required: true })}
           />
-          {errors.email && <span className=" text-red-600">Email is required</span>}
+          {errors.email && <span className=" text-red-600 text-xs lg:text-sm">Email is required</span>}
         </div>
         <div className="w-full flex-col justify-start items-start gap-2 inline-flex">
           <label className="self-stretch text-[#666666] text-sm lg:text-base font-semibold font-['DM Sans'] leading-[18px]">
@@ -152,7 +163,7 @@ const ContactUsFormComp: React.FC = () => {
           </label>
           <PhoneInput
             className={`self-stretch lg:h-14 p-3 lg:p-4 bg-white rounded-lg border ${
-              errors.phone ? "border-red-600" : "border-[#cccccc]"
+              errors.phone ? "border-red-600 text-xs lg:text-sm" : "border-[#cccccc]"
             } text-[#666666] text-sm font-normal font-['DM Sans'] leading-normal outline-none hover:border-btnColor focus:border-btnColor`}
             placeholder="Enter phone number"
             defaultCountry="BD"
@@ -162,22 +173,22 @@ const ContactUsFormComp: React.FC = () => {
         </div>
       </div>
 
-      <div className="self-stretch h-[329px] flex-col justify-start items-start gap-2 lg:gap-6 flex">
-        <div className="self-stretch h-[265px] flex-col justify-start items-start gap-4 flex">
-          <div className="self-stretch h-[159px] flex-col justify-start items-start gap-2 flex">
+      <div className="self-stretch flex-col justify-start items-start gap-2 lg:gap-6 flex">
+        <div className="self-stretch flex-col justify-start items-start gap-4 flex">
+          <div className="w-full min-h-[100px] flex flex-col justify-start items-start gap-2">
             <label className="self-stretch text-[#666666] text-sm lg:text-base font-semibold font-['Noto Sans'] leading-[18px]">
               How can we help you?
             </label>
             <textarea
-              className="self-stretch grow shrink basis-0 p-4 bg-white rounded-lg border border-[#cccccc] text-[#666666] text-sm font-normal  leading-normal outline-none hover:border-btnColor focus:border-btnColor"
+              className="w-full min-h-[100px] p-4 bg-white rounded-lg border border-[#cccccc] text-[#666666] text-sm font-normal leading-normal outline-none hover:border-btnColor focus:border-btnColor resize-none"
               placeholder="Type here"
+              rows={4}
               {...register("message", { required: true })}
             />
-            {errors.message && <span className=" text-red-600">Message is required</span>}
+            {errors.message && <span className="text-red-600 text-xs lg:text-sm">Message is required</span>}
           </div>
 
           <div className="justify-start items-start gap-2.5 inline-flex flex-col">
-            {/* Hidden file input */}
             <input
               type="file"
               id="file-upload"
@@ -186,7 +197,6 @@ const ContactUsFormComp: React.FC = () => {
               {...register("attachment", { onChange: handleFileChange })}
             />
             {errors.attachment && <span className=" text-red-600">This field is required</span>}
-            {/* Clickable label */}
             <div className=" flex flex-row justify-center items-start lg:items-center gap-4 lg:gap-2.5">
               <label htmlFor="file-upload" className="flex items-center gap-2 cursor-pointer">
                 <div className="flex lg:items-center gap-1 text-[#5856d6]">
@@ -197,40 +207,44 @@ const ContactUsFormComp: React.FC = () => {
                 </div>
               </label>
 
-              {/* Additional info */}
               <div className="text-[#5856d6] text-[10px] font-medium font-['DM Sans']">
                 <li>Max Size: 25MB per file</li>
                 <li>Supported Formats: .jpg, .png, .pdf, .docx</li>
               </div>
             </div>
 
-            {/* Display list of uploaded files */}
             {files.length > 0 && (
-              <ul className="mt-2 text-[#666666] text-sm font-normal font-['DM Sans'] list-disc list-inside">
-                {files.map((file, index) => (
-                  <li
-                    key={index}
-                    className="font-medium flex flex-row justify-start text-red-600 items-center gap-2"
-                  >
-                    {file.name}
-
-                    <button
-                      onClick={() => {
-                        setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
-                      }}
+              <>
+                <ul className="mt-2 text-[#666666] text-sm font-normal font-['DM Sans'] list-disc list-inside flex flex-wrap gap-3">
+                  {files.map((file, index) => (
+                    <li
+                      key={index}
+                      className={`font-medium flex flex-row justify-start items-center gap-1 ${
+                        totalSize >= 25 * 1024 * 1024 ? "text-red-600" : "text-green-600"
+                      }`}
                     >
-                      <i className="fa-solid fa-circle-xmark fa-lg" style={{ color: "#df3a4a" }}></i>
-                    </button>
-                  </li>
-                ))}
-              </ul>
+                      {file.name}
+                      <button
+                        onClick={() => {
+                          setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+                        }}
+                      >
+                        <i className="fa-solid fa-circle-xmark fa-lg hover:text-[#df3a4a]"></i>
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+                <p className="text-xs text-[#666666] font-medium mt-1">
+                  Total Size: {formatBytes(totalSize)}
+                </p>
+              </>
             )}
           </div>
 
           <div className="self-stretch py-3 justify-start items-center gap-2.5 inline-flex">
             <div className="flex gap-4">
               <input
-                {...register("ndaCheckbox", { required: "You must agree to the NDA" })} // register the checkbox with validation
+                {...register("ndaCheckbox", { required: "You must agree to the NDA" })}
                 type="checkbox"
                 className="toggle toggle-primary [--tglbg:white]"
                 id="nda-checkbox"
@@ -242,9 +256,8 @@ const ContactUsFormComp: React.FC = () => {
             </div>
           </div>
 
-          {/* Display error message */}
           {errors.ndaCheckbox && (
-            <div className="text-red-500 text-sm -mt-4">{errors.ndaCheckbox.message}</div>
+            <div className="text-red-500 text-xs lg:text-sm -mt-4">{errors.ndaCheckbox.message}</div>
           )}
         </div>
 
